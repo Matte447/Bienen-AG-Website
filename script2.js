e25cbb3e41c635e8960904ee8a9d1d5f7896309c
Matte447 <matteo.bienert@icloud.com>
2025-07-22 11:19:17 +0200

    Refactor CSS styles for consistency and readability; update HTML structure for better semantics and accessibility

--- script2.js
+++ script2.js
@@ -1,29 +1,28 @@
-let num = 1
-let fotos = 6
+let num = 1;
+let fotos = 6;
 
+function Forward() {
+  console.log("Test");
+  num++;
+  if (num < 1) {
+    num = fotos;
+  } else if (num > fotos) {
+    num = 1;
+  }
 
-function Forward(){
-    console.log("Test")
-    num++
-    if(num<1){
-        num = fotos
-    }else if(num>fotos){
-        num = 1
-    }
-
-    document.getElementsByClassName("switchfoto")[0].src = "img/AboutUs" + num + ".JPG"
-    
+  document.getElementsByClassName("switchfoto")[0].src =
+    "img/AboutUs" + num + ".JPG";
 }
 
-function Back(){
-    console.log("Test")
-    num--
-    if(num<1){
-        num = fotos
-    }else if(num>fotos){
-        num = 1
-    }
+function Back() {
+  console.log("Test");
+  num--;
+  if (num < 1) {
+    num = fotos;
+  } else if (num > fotos) {
+    num = 1;
+  }
 
-    document.getElementsByClassName("switchfoto")[0].src = "img/AboutUs" + num + ".JPG"
-    
+  document.getElementsByClassName("switchfoto")[0].src =
+    "img/AboutUs" + num + ".JPG";
 }
